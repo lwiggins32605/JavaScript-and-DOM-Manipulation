4d2e53674790dfc86f48c629f0d179b5aa4272f2
Verna Orsatti <pccassistants@gmail.com>
2018-08-26 22:50:40 -0600

    Update all levels with back_up files
    Included in the code is the Level 1: code to just input date for return
    The current:  app.js and index.html files are coordinated to work

---
+++ StarterCode/static/js/backup_app.js
@@ -0,0 +1,54 @@
+// Data from data.js
+var alienData = data;
+
+var tbody = d3.select("tbody");
+
+var table = d3.select("table");
+
+function resetForm() {
+    document.getElementById("myForm").reset();
+  }
+
+table.attr("class", "table table-striped");
+
+// Select Submit button with id = "filter-btn" changed to #submit
+var submit = d3.select("#submit");
+
+submit.on("click", function() {
+
+    
+    //document.getElementById("myForm").reset();
+    
+    // Prevent page from refreshing - from ex 09
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    
+    // Get the property of the input element
+    var inputValue = inputElement.property("value");
+    //console.log(inputValue);
+    //console.log(alienData);
+
+    // Filter
+    var filteredData = alienData.filter(siting => siting.datetime === inputValue);
+    //console.log(filteredData)
+
+    filteredData.forEach(x => {
+         var row = tbody.append("tr");
+
+        row.append("td").text(x.datetime);
+        row.append("td").text(x.city);
+        row.append("td").text(x.state);
+        row.append("td").text(x.country);
+        row.append("td").text(x.shape);
+        row.append("td").text(x.durationMinutes);
+        row.append("td").text(x.comments);
+
+    })
+    
+});
+
+
+// "All of life is calculus: every thing, every movement, every thought" ~ Verna Orsatti
+
